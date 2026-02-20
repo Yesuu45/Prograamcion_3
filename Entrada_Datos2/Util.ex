51144dcc0af7c24f3524ac9c927d9d6fe6798521
Yesuu45 <yesuue.arost@uqvirtual.edu.co>
2026-02-20 00:41:48 -0500

    cambios

--- Entrada_Datos2/Util.ex
+++ Entrada_Datos2/Util.ex
@@ -1,21 +1,21 @@
 defmodule Util do
-  # Función para ingresar texto usando Java
+  # Pide input usando Java
   def ingresar(mensaje, :texto) do
     case System.cmd("java", ["-cp", ".", "Mensaje", "input", mensaje]) do
       {output, 0} ->
         IO.puts("Texto ingresado correctamente en Java.")
-        IO.puts("Entrada Java: #{output}")
         String.trim(output)
 
       {error, code} ->
-        IO.puts("Error al ejecutar el programa Java. Código: #{code}")
+        IO.puts("Error al ejecutar el programa Java. Código de salida: #{code}")
         IO.puts("Mensaje de error: #{error}")
         nil
     end
   end
 
-  # Función para mostrar mensaje usando Java
+  # Muestra mensaje usando Java
   def mostrar_mensaje(mensaje) do
+    # Aquí no es input, solo mostramos el mensaje
     System.cmd("java", ["-cp", ".", "Mensaje", mensaje])
   end
 end
